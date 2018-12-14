4a296271ff8e67ece2861851af9c284237a71cad
Reed <roneal20@gmail.com>
2018-12-14 15:56:11 -0500

    creation

---
+++ src/main/java/com/techtalentsouth/hrdb/HrdbApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.hrdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrdbApplication.class, args);
+	}
+
+}
+
