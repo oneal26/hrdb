e048da83c16def30b992b619a688f80361fdbe02
Reed <roneal20@gmail.com>
2018-12-15 17:01:33 -0500

    switched first view you see when accessing site to login. began to populate login html

--- src/main/java/com/techtalentsouth/hrdb/Employees/EmployeeController.java
+++ src/main/java/com/techtalentsouth/hrdb/Employees/EmployeeController.java
@@ -3,6 +3,9 @@ package com.techtalentsouth.hrdb.Employees;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.techtalentsouth.TechTalentBlog.BlogPost.BlogPost;
 
 @Controller
 public class EmployeeController {
@@ -10,12 +13,19 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	@GetMapping("/")
-	public String index() {
-		return "index.html";
+	@GetMapping("index")
+	public ModelAndView index(Employee employee) {
+		ModelAndView mv = new ModelAndView("employee/index");
+		mv.addObject("employees", employeeRepository.findAll());
+		return mv;
 	}
 	
-	
+	@GetMapping("/")
+		public ModelAndView login(Employee employee) {
+			ModelAndView mv = new ModelAndView("employee/login");
+			return mv;
+		}
+	}
 	
 
-}
+
