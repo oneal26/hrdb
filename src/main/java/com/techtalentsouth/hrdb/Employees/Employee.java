13b77180007b513bc7389aab58f7d99cb85f5183
kelvoNgash <wamahenya82@gmail.com>
2018-12-15 14:26:05 -0500

    new changes

--- src/main/java/com/techtalentsouth/hrdb/Employees/Employee.java
+++ src/main/java/com/techtalentsouth/hrdb/Employees/Employee.java
@@ -15,5 +15,89 @@ public class Employee {
 	private String lastName;
 	private Boolean isAdmin;
 	private Integer hrsWorked;
+	private Long passWord;
+	
+	
+	public Employee() {
+		//Empty Employee constructor for the JPA
+		
+	}
+	
+	public Employee(String firstName, String lastName, Boolean isAdmin, Integer hrsWorked) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.isAdmin = isAdmin;
+		
+	}
+	
+	//This method is for checking whether the person loggin as admin is admin 
+	//incase we need this for loggin security
+	public Boolean admin() {
+		  if(firstName =="Mike" && lastName =="Smith" && passWord == passWord) {
+			  return true;
+			  
+		  }else
+			  
+			  return false;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public Integer getHrsWorked() {
+		return hrsWorked;
+	}
+
+	public void setHrsWorked(Integer hrsWorked) {
+		this.hrsWorked = hrsWorked;
+	}
+
+	public Long getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(Long passWord) {
+		this.passWord = passWord;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
