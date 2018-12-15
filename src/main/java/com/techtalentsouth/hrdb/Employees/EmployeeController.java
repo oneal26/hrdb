ef6b3dd1a81fefdb6b00a5bf12d7ba6bd5390711
LatoriMiller <latori.miller@gmail.com>
2018-12-15 12:50:31 -0500

    add index route and annotate Controller

--- src/main/java/com/techtalentsouth/hrdb/Employees/EmployeeController.java
+++ src/main/java/com/techtalentsouth/hrdb/Employees/EmployeeController.java
@@ -1,5 +1,14 @@
 package com.techtalentsouth.hrdb.Employees;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EmployeeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index.html";
+	}
 
 }
