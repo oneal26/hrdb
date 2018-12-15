2fcbb67b73c906ef9a1a1a01343382a6e21c1ed2
kelvoNgash <wamahenya82@gmail.com>
2018-12-15 13:36:43 -0500

    Changes on .java file

--- src/main/java/com/techtalentsouth/hrdb/Employees/Employee.java
+++ src/main/java/com/techtalentsouth/hrdb/Employees/Employee.java
@@ -1,5 +1,19 @@
 package com.techtalentsouth.hrdb.Employees;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Boolean isAdmin;
+	private Integer hrsWorked;
 
 }
