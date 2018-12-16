5ed73bce999be2a8f1df1573a57be886b369729d
Reed <roneal20@gmail.com>
2018-12-15 19:27:09 -0500

    fixed error

--- src/main/java/com/techtalentsouth/hrdb/Employees/EmployeeController.java
+++ src/main/java/com/techtalentsouth/hrdb/Employees/EmployeeController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.techtalentsouth.TechTalentBlog.BlogPost.BlogPost;
-
 @Controller
 public class EmployeeController {
 	
@@ -27,5 +25,5 @@ public class EmployeeController {
 		}
 	}
 	
-
+	
 
